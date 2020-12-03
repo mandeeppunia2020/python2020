b1ab8a791d50154e1d367a2017a6d0923edefa8b
mandeeppunia2020 <75325111+mandeeppunia2020@users.noreply.github.com>
2020-12-03 15:44:04 +0530

    Add files via upload

---
+++ Day8.py
@@ -0,0 +1,184 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+Intronduction to tuple datatype:
+
+
+# In[ ]:
+
+
+defination: an immutable list is called tuple:
+
+classfication: tuple is classified as an immutable datatype:
+
+how to define the tuple:------>()        
+
+
+# In[1]:
+
+
+student=('mandeep','punia','muskan','ravi','rohit','prince')
+
+print(student)
+
+
+# In[2]:
+
+
+type(student)
+
+
+# In[ ]:
+
+
+Req: in muskan place i want to replace whith anu....!
+
+
+# In[3]:
+
+
+student[2]= 'anu' # in which if you declaried a tuple that means no one change it.....!
+
+
+# In[ ]:
+
+
+For Example:
+
+
+# In[4]:
+
+
+dimensions=(220,100)
+
+
+# In[6]:
+
+
+print(dimensions)
+
+
+# In[ ]:
+
+
+Req: i want to change 220 into 300..!
+
+
+# In[9]:
+
+
+dimensions[0]= 300 # hence once you create a tuple you not do any change in it...!
+
+
+# In[10]:
+
+
+print(dimensions)
+
+
+# In[ ]:
+
+
+
+
+
+# In[11]:
+
+
+print(student)
+
+
+# In[ ]:
+
+
+For loop in tuple:
+
+
+# In[13]:
+
+
+for x in student:
+    print(x)
+
+
+# In[15]:
+
+
+for x in student:
+    print(x.title())
+
+
+# In[ ]:
+
+
+Introduction to Dictionary datatpe:
+
+
+# In[16]:
+
+
+defination: A dictionary is a combination odf key-value pairs.
+ 
+classification: it is classified as mutable datatype.
+    
+how to define-----> {}#curley brackeys...!   
+
+
+# In[ ]:
+
+
+Req: i want to create a alien game and add properties of it color and points...!
+
+
+# In[ ]:
+
+
+
+
+
+# In[17]:
+
+
+alien={'color':'green','points':5}
+
+print(alien)
+
+
+# In[18]:
+
+
+type(alien)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
