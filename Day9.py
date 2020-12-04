a695f4bfc252d6a5927f963bd440038b6e058ddc
mandeeppunia2020 <75325111+mandeeppunia2020@users.noreply.github.com>
2020-12-04 20:54:58 +0530

    Add files via upload

---
+++ Day9.py
@@ -0,0 +1,234 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+defination: A dictionary is a combination of key-value pairs.
+ 
+classification: it is classified as mutable datatype.
+    
+how to define-----> {}#curley brackets...!   
+
+
+# In[ ]:
+
+
+
+
+
+# In[1]:
+
+
+alein={'color':'green','piont':5}# key(color):value(green),key(point):value(5)
+
+
+# In[2]:
+
+
+print(alein)
+
+
+# In[3]:
+
+
+type(alein)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+Req: How to access the value form the above dictionary
+
+
+# In[4]:
+
+
+print(alein['color'])# By giving the key we are getting the output as value....!
+
+
+# In[5]:
+
+
+print(alein['piont'])
+
+
+# In[ ]:
+
+
+Req: can we get the key by giving the values...**
+
+
+# In[6]:
+
+
+print(alein['green'])
+
+
+# In[ ]:
+
+
+** Give the key and get the value...>>>
+
+
+# In[ ]:
+
+
+Can i give the key and get the value---->> No key error...
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+Req: how to add new key value pair to the dictionary.....!
+
+
+# In[7]:
+
+
+print(alein)
+
+
+# In[ ]:
+
+
+mandeep:22......muskan:21
+
+
+# In[8]:
+
+
+alein['mandeep']=22
+
+
+# In[9]:
+
+
+alein['muskan']=21
+
+
+# In[10]:
+
+
+print(alein)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+facebook account:
+
+
+# In[11]:
+
+
+user={'username':'mandeep123','fname':'mandeep','lname':'punia','dob':'26-04-1999','pwd':'1233'}
+
+
+# In[12]:
+
+
+print(user)
+
+
+# In[13]:
+
+
+user['location']='Haryana'
+
+
+# In[14]:
+
+
+print(user)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+Req: how to delete a key value pair from a dictionary....!
+
+
+# In[15]:
+
+
+print(user)
+
+
+# In[ ]:
+
+
+Req: i want to delete dob  from the above...!
+
+
+# In[16]:
+
+
+del user['dob']
+
+
+# In[17]:
+
+
+print(user)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
